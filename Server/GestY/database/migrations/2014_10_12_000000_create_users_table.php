1ccf4817a8bc05351180e7509bc39bcad421f84d
Javier Garcia <84515834+glyaxz@users.noreply.github.com>
2023-09-05 08:49:16 +0200

    Last Commit

--- Server/GestY/database/migrations/2014_10_12_000000_create_users_table.php
+++ Server/GestY/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->integer('empleado_id')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role', ['admin', 'empleado'])->default('empleado');
